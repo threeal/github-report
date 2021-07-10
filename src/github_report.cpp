00ead2bfef0acae92ea3b872c450acf33fc5a667
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-07-10 10:41:00 +0700

    feat: use nlohmann json to parse json
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/github_report.cpp
+++ src/github_report.cpp
@@ -2,8 +2,12 @@
 #include <curlpp/Easy.hpp>
 #include <curlpp/Exception.hpp>
 #include <curlpp/Options.hpp>
+#include <nlohmann/json.hpp>
 
 #include <iostream>
+#include <sstream>
+
+using nlohmann::json;
 
 int main(int argc, char ** argv)
 {
@@ -15,6 +19,7 @@ int main(int argc, char ** argv)
   std::string username(argv[1]);
 
   try {
+    std::cout << "generating report..." << std::endl;
     curlpp::Cleanup cleaner;
     curlpp::Easy request;
 
@@ -25,7 +30,13 @@ int main(int argc, char ** argv)
       "User-Agent: github-report"
     });
 
-    std::cout << request << std::endl;
+    std::stringstream ss;
+
+    ss << request;
+
+    auto json = json::parse(ss.str());
+
+    std::cout << json.dump(2) << std::endl;
   } catch (curlpp::LogicError & e) {
     std::cout << e.what() << std::endl;
     return 1;
